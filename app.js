382e80b86c6515da966a37ae6fdd3b420a41a548
wellfrog <wellfrog16@gmail.com>
2018-03-15 11:36:23 +0800

    测试跨域

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ require('./utils/prototype');
 
 // 允许跨域配置
 app.all('*', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    // res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
