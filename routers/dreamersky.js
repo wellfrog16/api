e770430e13346ae3d878ef3f7ccf4631e38f3f71
wellfrog <wellfrog16@gmail.com>
2018-09-12 11:20:18 +0800

    增加mockjs

--- routers/dreamersky.js
+++ routers/dreamersky.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const model = require('../models/usr/dreamersky');
+const Mock = require('mockjs');
+const utils = require('../utils/utils');
 const router = express.Router();
 
 router.post('/blog', (req, res, next) => model.blog.insert(req, res, next));
@@ -10,4 +12,16 @@ router.delete('/blog/:id(\\d+)', (req, res, next) => model.blog.delete(req, res,
 router.get('/blog', (req, res, next) => model.blog.list(req, res, next));
 router.get('/blog/:id(\\d+)', (req, res, next) => model.blog.detail(req, res, next));
 
+router.get('/test', (req, res, next) => {
+    const data = Mock.mock({
+        'total': 20,
+        'list|10-20': [{
+            'id|+1': 1,
+            'title': '@ctitle(15, 20)',
+            'date': '@datetime'
+        }]
+    });
+
+    utils.handle.sendSuccess(res, data)
+})
 module.exports = router;
